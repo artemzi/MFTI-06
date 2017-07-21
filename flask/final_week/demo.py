bb7d7e71370c92111dd7307b0c1e998eab10b38c
Artem Zinoviev <arzinoviev@gmail.com>
2017-07-21 11:05:51 +0300

    finish task from 7 week

--- flask/final_week/demo.py
+++ flask/final_week/demo.py
@@ -14,23 +14,16 @@ print(time.time() - start_time, "seconds")
 
 
 @app.route("/", methods=["POST", "GET"])
-def index_page(text="", prediction_message=""):
-    if request.method == "POST":
-        text = request.form["text"]
-        sys.stdout = open("ydf_demo_logs.txt", "a", "utf-8")
-        print(text)
-        print("<response> sys.stdout")
-        prediction_message = clf.get_prediction_message(text)
-        print(prediction_message)
-        print("<response> sys.stdout")
-
-    return render_template('hello.html', text=text, prediction_message=prediction_message)
+def index_page():
+    return render_template('hello.html')
 
 
-@app.route('/predict', methods=["POST"])
+@app.route('/predict')
 def predict(text=''):
-    txt = request.args.get('text')
-    return clf.get_prediction(txt)
+    text = request.args.get('text')
+    prediction_message = clf.get_prediction_message(text)
+    print(prediction_message)
+    return prediction_message
 
 
 @app.route('/<path:path>')
@@ -39,4 +32,4 @@ def hello(path=''):
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=False)
